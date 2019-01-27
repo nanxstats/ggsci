cf8d7d210367ff7737f1bf07a75e6eba1bd9ea1f
Nan Xiao <road2stat@gmail.com>
2019-01-27 15:02:20 -0500

    use roxygen2 package man page

--- R/ggsci-package.R
+++ R/ggsci-package.R
@@ -1,17 +1,2 @@
-#' Scientific Journal and Sci-Fi Themed Color Palettes for 'ggplot2'
-#'
-#' Scientific Journal and Sci-Fi Themed Color Palettes for 'ggplot2'
-#'
-#' Opened the vignette via \code{vignette("ggsci")}.
-#'
-#' \tabular{ll}{
-#' Package: \tab ggsci\cr
-#' Type: \tab Package\cr
-#' License: \tab GPL-3}
-#'
-#' @name ggsci-package
-#' @docType package
-#' @author
-#' Nan Xiao <\email{me@@nanx.me}>
-#' Miaozhu Li <\email{miaozhu.li@@duke.edu}>
-NULL
+#' @keywords internal
+"_PACKAGE"
