2b151e3f85e3a819058b6d982d30c416bfe55aad
Josh Cook <joshuacook0023@gmail.com>
2019-02-21 13:19:18 -0500

    feat: use COSMIC colors for hallmarks of cancer and mutational signatures

---
+++ R/discrete-cosmic.R
@@ -0,0 +1,106 @@
+#' COSMIC Color Palettes
+#'
+#' Color palettes inspired by the colors used projects from the
+#' \href{https://cancer.sanger.ac.uk/cosmic}{Catalogue Of
+#' Somatic Mutations in Cancers (COSMIC)}
+#'
+#' @param palette Palette type.
+#' Currently there is two available options: \code{"signature_substitutions"}
+#' (6-color palette) and \code{"hallmarks"} (10-color palette).
+#' @param alpha Transparency level, a real number in (0, 1].
+#' See \code{alpha} in \code{\link[grDevices]{rgb}} for details.
+#'
+#' @export pal_cosmic
+#'
+#' @importFrom grDevices col2rgb rgb
+#' @importFrom scales manual_pal
+#'
+#' @author Nan Xiao <\email{me@@nanx.me}> |
+#' <\href{https://nanx.me}{https://nanx.me}>
+#' @author Joshua H. Cook <\email{joshuacook0023@@gmail.com}> |
+#' <\href{https://github.com/jhrcook}{GitHub/jhrcook}>
+#'
+#' @examples
+#' library("scales")
+#' show_col(pal_cosmic("hallmark")(10))
+#' show_col(pal_cosmic("hallmark", alpha = 0.6)(10))
+#' show_col(pal_cosmic("signature_substitutions")(6))
+#' show_col(pal_cosmic("signature_substitutions", alpha = 0.6)(6))
+pal_cosmic <- function(palette = c("hallmarks"), alpha = 1) {
+  palette <- match.arg(palette)
+
+  if (alpha > 1L | alpha <= 0L) stop("alpha must be in (0, 1]")
+
+  raw_cols <- ggsci_db$"cosmic"[[palette]]
+  raw_cols_rgb <- col2rgb(raw_cols)
+  alpha_cols <- rgb(
+    raw_cols_rgb[1L, ], raw_cols_rgb[2L, ], raw_cols_rgb[3L, ],
+    alpha = alpha * 255L, names = names(raw_cols),
+    maxColorValue = 255L
+  )
+
+  manual_pal(unname(alpha_cols))
+}
+
+#' COSMIC Color Scales
+#'
+#' See \code{\link{pal_cosmic}} for details.
+#'
+#' @inheritParams pal_cosmic
+#' @param ... additional parameters for \code{\link[ggplot2]{discrete_scale}}
+#'
+#' @export scale_color_cosmic
+#'
+#' @importFrom ggplot2 discrete_scale
+#'
+#' @author Nan Xiao <\email{me@@nanx.me}> |
+#' <\href{https://nanx.me}{https://nanx.me}>
+#' @author Joshua H. Cook <\email{joshuacook0023@@gmail.com}> |
+#' <\href{https://github.com/jhrcook}{GitHub/jhrcook}>
+#'
+#' @rdname scale_cosmic
+#'
+#' @examples
+#'
+#' # TODO: change to useing this palette
+#'
+#' library("ggplot2")
+#' data("diamonds")
+#'
+#' ggplot(
+#'   subset(diamonds, carat >= 2.2),
+#'   aes(x = table, y = price, colour = cut)
+#' ) +
+#'   geom_point(alpha = 0.7) +
+#'   geom_smooth(method = "loess", alpha = 0.1, size = 1, span = 1) +
+#'   theme_dark() + theme(
+#'     panel.background = element_rect(fill = "#2D2D2D"),
+#'     legend.key = element_rect(fill = "#2D2D2D")
+#'   ) +
+#'   scale_color_cosmic()
+#'
+#' ggplot(
+#'   subset(diamonds, carat > 2.2 & depth > 55 & depth < 70),
+#'   aes(x = depth, fill = cut)
+#' ) +
+#'   geom_histogram(colour = "black", binwidth = 1, position = "dodge") +
+#'   theme_dark() + theme(
+#'     panel.background = element_rect(fill = "#2D2D2D")
+#'   ) +
+#'   scale_fill_cosmic()
+scale_color_cosmic <- function(palette = c("hallmarks"), alpha = 1, ...) {
+  palette <- match.arg(palette)
+  discrete_scale("colour", "cosmic", pal_cosmic(palette, alpha), ...)
+}
+
+#' @export scale_colour_cosmic
+#' @rdname scale_cosmic
+scale_colour_cosmic <- scale_color_cosmic
+
+#' @export scale_fill_cosmic
+#' @importFrom ggplot2 discrete_scale
+#' @rdname scale_cosmic
+scale_fill_cosmic <- function(palette = c("hallmarks"), alpha = 1, ...) {
+  palette <- match.arg(palette)
+  discrete_scale("fill", "cosmic", pal_cosmic(palette, alpha), ...)
+}
